4d92d0bea68c4452eb310265f28afdb770a161e1
Jochen Dueckminor <jochen@dueckminor.de>
2025-04-12 19:33:58 +0200

    Test pr (#5)
    * updated README.md
    
    * try to make it configurable
    
    * try to make it configurable
    
    * allow tests
    
    * update existing comment
    
    * build
    
    * add button to trigger the action again

--- pr-add-checkbox/dist/index.js
+++ pr-add-checkbox/dist/index.js
@@ -32591,11 +32591,29 @@ var __importStar = (this && this.__importStar) || (function () {
     };
 })();
 Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.createBody = createBody;
 const core = __importStar(__nccwpck_require__(7484));
 const github = __importStar(__nccwpck_require__(3228));
+function createBody(checkboxes) {
+    let body = ``;
+    if (!checkboxes) {
+        return body;
+    }
+    for (const checkbox of checkboxes.split(",")) {
+        const parts = checkbox.split("=");
+        let label = parts[0].trim();
+        let name = label;
+        if (parts.length == 2) {
+            name = parts[1].trim();
+        }
+        body += `- [ ] <!--${label}--> ${name}\n`;
+    }
+    return body;
+}
 async function run() {
     try {
         const token = core.getInput("github-token", { required: true });
+        const checkboxes = core.getInput("checkboxes", { required: true });
         const octokit = github.getOctokit(token);
         const context = github.context;
         if (context.eventName !== "pull_request") {
@@ -32608,17 +32626,32 @@ async function run() {
             core.setFailed("Pull request number is undefined.");
             return;
         }
-        const body = `
-## Select which tests to run
-- [ ] Run expensive integration tests
-- [ ] Run UI regression tests
-`;
-        await octokit.rest.issues.createComment({
+        let body = `<!-- pr-add-checkbox -->\n`;
+        body += `## Select which tests to run:\n`;
+        body += createBody(checkboxes);
+        body += `\n\n[Re-run this action](# "Re-run this action by re-triggering the workflow.")\n`;
+        const comments = await octokit.rest.issues.listComments({
             owner,
             repo,
             issue_number,
-            body,
         });
+        const existingComment = comments.data.find(comment => comment.body?.startsWith("<!-- pr-add-checkbox -->"));
+        if (existingComment) {
+            await octokit.rest.issues.updateComment({
+                owner,
+                repo,
+                comment_id: existingComment.id,
+                body,
+            });
+        }
+        else {
+            await octokit.rest.issues.createComment({
+                owner,
+                repo,
+                issue_number,
+                body,
+            });
+        }
         core.info("Comment with checkboxes posted.");
     }
     catch (error) {
--- pr-add-checkbox/src/index.ts
+++ pr-add-checkbox/src/index.ts
@@ -1,9 +1,27 @@
 import * as core from "@actions/core";
 import * as github from "@actions/github";
 
+export function createBody(checkboxes: string): string {
+  let body = ``;
+  if (!checkboxes) {
+    return body;
+  }
+  for (const checkbox of checkboxes.split(",")) {
+    const parts = checkbox.split("=")
+    let label = parts[0].trim();
+    let name = label
+    if (parts.length == 2) {
+      name = parts[1].trim();
+    }
+    body += `- [ ] <!--${label}--> ${name}\n`;
+  }
+  return body;
+}
+
 async function run() {
   try {
     const token = core.getInput("github-token", { required: true });
+    const checkboxes = core.getInput("checkboxes", { required: true });
     const octokit = github.getOctokit(token);
     const context = github.context;
 
@@ -20,18 +38,36 @@ async function run() {
       return;
     }
 
-    const body = `
-## Select which tests to run
-- [ ] Run expensive integration tests
-- [ ] Run UI regression tests
-`;
+    let body = `<!-- pr-add-checkbox -->\n`;
+    body += `## Select which tests to run:\n`
+    body += createBody(checkboxes);
+    body += `\n\n[Re-run this action](# "Re-run this action by re-triggering the workflow.")\n`;
 
-    await octokit.rest.issues.createComment({
+    const comments = await octokit.rest.issues.listComments({
+      owner,
+      repo,
+      issue_number,
+    });
+
+    const existingComment = comments.data.find(comment =>
+      comment.body?.startsWith("<!-- pr-add-checkbox -->")
+    );
+
+    if (existingComment) {
+      await octokit.rest.issues.updateComment({
+      owner,
+      repo,
+      comment_id: existingComment.id,
+      body,
+      });
+    } else {
+      await octokit.rest.issues.createComment({
       owner,
       repo,
       issue_number,
       body,
-    });
+      });
+    }
 
     core.info("Comment with checkboxes posted.");
   } catch (error: any) {
